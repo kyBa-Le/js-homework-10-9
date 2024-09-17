51d7b063f5913c020a2f62571d123982267c615a
kyBa-Le <lekyba2000hc@gmail.com>
2024-09-17 10:54:05 +0700

    Bai kiem tra ver1

---
+++ kiemtra/script.js
@@ -0,0 +1,34 @@
+
+function Add(){
+    var pimg = document.getElementById("p-img").value;
+    var pname = document.getElementById("p-name").value;
+    var newPrice = document.getElementById("in-new-price").value;
+    var oldPrice = document.getElementById("in-old-price").value;
+    console.log(newPrice);
+    console.log(oldPrice);
+    var type = document.getElementById("type").value;
+    var pcode = document.getElementById("p-code").value;
+    var node = document.querySelectorAll(".card")[0];
+    var clone = node.cloneNode(true);
+    clone.querySelector("img").src = pimg;
+    var cardBody = clone.querySelector(".card-body")
+    cardBody.querySelector(".card-title").innerHTML = pname + " - " + pcode;
+    cardBody.querySelector(".price").querySelector("#new-price").textContent = newPrice;
+    cardBody.querySelector(".price").querySelector("#old-price").textContent = oldPrice;
+    document.querySelector(".product").appendChild(clone);
+}
+
+function Clear(){
+    const input = document.querySelectorAll("input");
+    input.forEach(element => {
+        element.value = "";
+    });
+}
+function Close(){
+    document.querySelector(".form").style.display = 'none';
+    document.querySelector(".container").style.filter = 'none';
+}
+function openForm(){
+    document.querySelector(".form").style.display = 'flex';
+    document.querySelector(".container").style.filter = 'blur(5px)';
+}
