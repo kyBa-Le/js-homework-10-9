9ae9d91326d6097622f5805341a95d0411292521
kyBa-Le <lekyba2000hc@gmail.com>
2024-09-17 15:49:07 +0700

    This exercise is done

--- kiemtra/script.js
+++ kiemtra/script.js
@@ -4,24 +4,33 @@ function Add(){
     var pname = document.getElementById("p-name").value;
     var newPrice = document.getElementById("in-new-price").value;
     var oldPrice = document.getElementById("in-old-price").value;
-    console.log(newPrice);
-    console.log(oldPrice);
     var type = document.getElementById("type").value;
     var pcode = document.getElementById("p-code").value;
-    var node = document.querySelectorAll(".card")[0];
-    var clone = node.cloneNode(true);
-    clone.querySelector("img").src = pimg;
-    var cardBody = clone.querySelector(".card-body")
-    cardBody.querySelector(".card-title").innerHTML = pname + " - " + pcode;
-    cardBody.querySelector(".price").querySelector("#new-price").textContent = newPrice;
-    cardBody.querySelector(".price").querySelector("#old-price").textContent = oldPrice;
-    var productType;
-    if(type="men"){
-        productType = "men-fashion";
-    }else if(type="women"){
-        productType = "women-fashion";
+    var valid = true;
+    const input = document.getElementsByTagName("input");
+    Array.prototype.slice.call(input, 1, input.length).forEach(element => {
+        console.log(element.value);
+        if(element.value == ""){
+            valid = false;
+        }
+    });
+    if(valid == false){
+        window.alert("Vui lòng nhập đầy đủ thông tin!");
+    }else{
+        var node = document.querySelectorAll(".card")[0];
+        var clone = node.cloneNode(true);
+        clone.querySelector("img").src = pimg;
+        var cardBody = clone.querySelector(".card-body");
+        cardBody.querySelector(".card-title").innerHTML = pname + " - " + pcode;
+        cardBody.querySelector(".price").querySelector("#new-price").textContent = newPrice;
+        cardBody.querySelector(".price").querySelector("#old-price").textContent = oldPrice;
+        var productType;
+        if (type == "men") {
+            document.getElementById("men-fashion").querySelector(".product").appendChild(clone);
+        } else {
+            document.getElementById("women-fashion").querySelector(".product").appendChild(clone);
+        }
     }
-    document.getElementById(productType).querySelector(".product").appendChild(clone);
 }
 
 function Clear(){
